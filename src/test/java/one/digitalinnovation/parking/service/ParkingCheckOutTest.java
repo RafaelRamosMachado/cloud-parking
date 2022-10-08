ec5f80de24085018bdfb6922db020ca850079a13
RafaelRamosMachado <rafa.ramosmachado@gmail.com>
2022-10-08 00:36:58 -0300

    Adicionando Persistencia

---
+++ src/test/java/one/digitalinnovation/parking/service/ParkingCheckOutTest.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.parking.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ParkingCheckOutTest {
+
+	@Test
+	void testGetBill() {
+		
+	}
+
+}
